ba46d6b1bcbe2bd0f388332675b8daa517853d34
MaxWofford <max.speed.wofford@gmail.com>
2014-08-02 01:47:45 -0700

    Beautified js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,10 @@
-//Social button interactions go here
-	$(window).scroll(function(e) {
-		var rotation = $(document).scrollTop(); 
-		//Set span innerHTML to rolation
-		$('#test').text(rotation);
-		$('.social').css( "-webkit-transform", "rotate(" + rotation + "deg)" );
-		$('.social').css( "-moz-transform", "rotate(" + rotation + "deg)" );
-		$('.social').css( "-o-transform", "rotate(" + rotation + "deg)" );		
-	});
 
-//Animations 
-
-/* Example use of animate.css with jquery
-$( document ).ready(function(e){
-	$('SOMESELECTORHERE').addClass('animated ANIMATE.CSS-CLASS');
-});
-*/
+  $(window).scroll(function(e) {
+    var rotation = $(window).scrollTop(); 
+    //Set span innerHTML to rolation
+    $('#test').text(rotation);
+    //Social button interactions go here
+    $('.social').css( "-webkit-transform", "rotate(" + rotation + "deg)" );
+    $('.social').css( "-moz-transform", "rotate(" + rotation + "deg)" );
+    $('.social').css( "-o-transform", "rotate(" + rotation + "deg)" );
+  });
