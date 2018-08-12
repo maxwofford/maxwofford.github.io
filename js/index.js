d6c76f82449c63c7b949291f17f4fd94d79a99b3
Max Wofford <max@maxwofford.com>
2018-08-11 20:54:59 -0700

    Rewrite console check loop

--- js/index.js
+++ js/index.js
@@ -46,20 +46,26 @@ function setConsoleState(consoleState) {
   }
 }
 
-function checkLoop() {
-  const devtools = /./;
-  let open = false
+function isConsoleOpen() {
+  let isOpen = false
+  const devtools = /./
   devtools.toString = function() {
-    open = true;
+    isOpen = true;
   }
-
   console.log('%c', devtools)
-  const isFF = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
-  setConsoleState(!isFF && open)
+
+  // Unfortunately this detection method gives a false positive in FireFox, so I'm just disabling it for now
+  const isFF = navigator.userAgent.toLowerCase().indexOf('firefox') > -1
+  return !isFF && isOpen
+}
+
+function checkLoop() {
+  setConsoleState(isConsoleOpen())
   setTimeout(checkLoop, 200)
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
   window.consoleState = false
+  window.initialDetectedState = isConsoleOpen()
   checkLoop()
 })
