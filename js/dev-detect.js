a8077e13ce1592e5bbf8bb2f64f5a22e7ad220f2
Max Wofford <max@maxwofford.com>
2019-11-11 14:59:04 -0500

    Update with latest dev-detect.js

--- js/dev-detect.js
+++ js/dev-detect.js
@@ -5,57 +5,53 @@
 	by Sindre Sorhus
 	MIT License
 */
-;(function() {
-  'use strict'
-  var devtools = {
-    open: false,
-    orientation: null
-  }
-  var threshold = 160
-  var emitEvent = function(state, orientation) {
-    window.dispatchEvent(
-      new CustomEvent('devtoolschange', {
-        detail: {
-          open: state,
-          orientation: orientation
-        }
-      })
-    )
-  }
+(function () {
+	'use strict';
 
-  setInterval(function() {
-    var widthThreshold = window.outerWidth - window.innerWidth > threshold
-    var heightThreshold = window.outerHeight - window.innerHeight > threshold
-    var orientation = widthThreshold ? 'vertical' : 'horizontal'
+	const devtools = {
+		isOpen: false,
+		orientation: undefined
+	};
 
-    if (
-      !(heightThreshold && widthThreshold) &&
-      ((window.Firebug &&
-        window.Firebug.chrome &&
-        window.Firebug.chrome.isInitialized) ||
-        widthThreshold ||
-        heightThreshold)
-    ) {
-      if (!devtools.open || devtools.orientation !== orientation) {
-        emitEvent(true, orientation)
-      }
+	const threshold = 160;
 
-      devtools.open = true
-      devtools.orientation = orientation
-    } else {
-      if (devtools.open) {
-        emitEvent(false, null)
-      }
+	const emitEvent = (isOpen, orientation) => {
+		window.dispatchEvent(new CustomEvent('devtoolschange', {
+			detail: {
+				isOpen,
+				orientation
+			}
+		}));
+	};
 
-      devtools.open = false
-      devtools.orientation = null
-    }
-  }, 500)
+	setInterval(() => {
+		const widthThreshold = window.outerWidth - window.innerWidth > threshold;
+		const heightThreshold = window.outerHeight - window.innerHeight > threshold;
+		const orientation = widthThreshold ? 'vertical' : 'horizontal';
 
-  if (typeof module !== 'undefined' && module.exports) {
-    module.exports = devtools
-  } else {
-    window.devtools = devtools
-  }
+		if (
+			!(heightThreshold && widthThreshold) &&
+			((window.Firebug && window.Firebug.chrome && window.Firebug.chrome.isInitialized) || widthThreshold || heightThreshold)
+		) {
+			if (!devtools.isOpen || devtools.orientation !== orientation) {
+				emitEvent(true, orientation);
+			}
+
+			devtools.isOpen = true;
+			devtools.orientation = orientation;
+		} else {
+			if (devtools.isOpen) {
+				emitEvent(false, undefined);
+			}
+
+			devtools.isOpen = false;
+			devtools.orientation = undefined;
+		}
+	}, 500);
+
+	if (typeof module !== 'undefined' && module.exports) {
+		module.exports = devtools;
+	} else {
+		window.devtools = devtools;
+	}
 })()
-/* END DEVTOOLS-DETECT */
